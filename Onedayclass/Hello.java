3d56b3ca1ea8a7afdb99a94517403dde368801af
minho1234 <51068083+minho1234@users.noreply.github.com>
2019-05-27 13:47:07 +0900

    Add files via upload

---
+++ Onedayclass/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+public static void main(String[] args) {
+	
+	System.out.println("헬로");
+	System.out.println("hello");
+	System.out.println("hello!@#$%");
+	
+	System.out.println("eeeee");
+}
+}
